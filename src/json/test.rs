a3882c60d1744c8d942b0c76ad95571a67f9b3d6
Hugh Kaznowski <phkaznowski@gmail.com>
2024-12-29 21:12:15 +0000

    JSON object cmp test pass

--- src/json/test.rs
+++ src/json/test.rs
@@ -1,8 +1,9 @@
 use crate::ast::{RapidRecastDefinition, Version};
 use crate::json::JsonRRDL;
-use crate::ParseRRDL;
+use crate::test::bare_minimum_schema;
+use crate::{ParseRRDL, SaveRRDL};
+use serde_json::Value;
 use std::borrow::Cow;
-use crate::test::SCHEMA_BARE_MINIMUM;
 
 #[test]
 pub fn bare_minimum() {
@@ -41,6 +42,21 @@ pub fn bare_minimum() {
 
 #[test]
 pub fn test_basic_save() {
-    let _definition = SCHEMA_BARE_MINIMUM.clone();
-    todo!()
+    let definition = bare_minimum_schema();
+    let res = JsonRRDL {}.save_rrdl(&definition);
+    let res_str = String::from_utf8(res.into_inner()).unwrap();
+    let res: Value = serde_json::from_str(&res_str).unwrap();
+    let expected_str = r#"{
+        "id":"unique-schema-id-123",
+        "language_version":"1.2.3",
+        "file_version":"4.5.6",
+        "name":"some name",
+        "description":"some description",
+        "ast":[
+            {"ProtocolDefinition":{"protocol":"HTTP","sequence":0}}
+        ]}"#;
+    let expected: Value = serde_json::from_str(expected_str).unwrap();
+    assert!(expected.is_object());
+    assert!(res.is_object());
+    assert_eq!(res, expected);
 }
--- src/test.rs
+++ src/test.rs
@@ -1,19 +1,28 @@
-use std::borrow::Cow;
-use crate::ast::{RapidRecastDefinition, Version};
-
-pub const SCHEMA_BARE_MINIMUM: &'static RapidRecastDefinition = &RapidRecastDefinition {
-    id: Cow::Borrowed("unique-schema-id-123"),
-    language_version: Version {
-        major: 1,
-        minor: 2,
-        patch: 3,
-    },
-    file_version: Version {
-        major: 4,
-        minor: 5,
-        patch: 6,
-    },
-    name: Some(Cow::Borrowed("some name")),
-    description: Some(Cow::Borrowed("some description")),
-    ast: vec![],
+use crate::ast::{
+    ProtocolType, RapidAstStatement, RapidProtocolDefinition, RapidRecastDefinition, Version,
 };
+use std::borrow::Cow;
+
+pub fn bare_minimum_schema() -> RapidRecastDefinition<'static> {
+    RapidRecastDefinition {
+        id: Cow::Borrowed("unique-schema-id-123"),
+        language_version: Version {
+            major: 1,
+            minor: 2,
+            patch: 3,
+        },
+        file_version: Version {
+            major: 4,
+            minor: 5,
+            patch: 6,
+        },
+        name: Some(Cow::Borrowed("some name")),
+        description: Some(Cow::Borrowed("some description")),
+        ast: vec![RapidAstStatement::ProtocolDefinition(
+            RapidProtocolDefinition {
+                protocol: ProtocolType::HTTP,
+                sequence: 0,
+            },
+        )],
+    }
+}
